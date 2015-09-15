f99d069229029e6fdda267b0bc9abc199cf77738
kazuminn <warugaki_kazumi@docomo.ne.jp>
2015-09-15 21:12:14 +0900

    add refinments

--- Core.rb
+++ Core.rb
@@ -1,30 +1,36 @@
 require 'pp'
 
 class Core
+  module InternalMethods
+    refine Core do
+      def list_get
+        $member_text = $client.list_members($summorner_name,$from_list_name).attrs[:users]
+      end
+
+      def get_id
+        $new_members = []
+
+        $member_text.each do |user|
+          $new_members.push user[:id]
+        end
+      end
+
+      def list_create
+        $client.create_list($to_list_name)
+      end
+
+      def add
+        $client.add_list_members($to_list_name,$new_members)
+      end
+    end
+  end
+
+  using InternalMethods
+
   def run
     list_get
     get_id
     list_create
     add
-  end
-
-  def list_get
-    $member_text = $client.list_members($summorner_name,$from_list_name).attrs[:users]
-  end
-
-  def get_id
-    $new_members = []
-
-    $member_text.each do |user|
-      $new_members.push user[:id]
-    end
-  end
-
-  def list_create
-    $client.create_list($to_list_name)
-  end
-
-  def add
-    $client.add_list_members($to_list_name,$new_members)
   end
 end
